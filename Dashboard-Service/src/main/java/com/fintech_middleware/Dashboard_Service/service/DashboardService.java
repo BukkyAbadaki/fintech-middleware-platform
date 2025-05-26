45c198350dc4c1cc8c27ab362c2762fe81be189a
BukkyAbadaki <95274482+BukkyAbadaki@users.noreply.github.com>
2025-05-26 05:30:29 +0100

    Add Dashboard-Service file

---
+++ Dashboard-Service/src/main/java/com/fintech_middleware/Dashboard_Service/service/DashboardService.java
@@ -0,0 +1,42 @@
+package com.fintech_middleware.Dashboard_Service.service;
+
+import com.fintech_middleware.Dashboard_Service.domain.Account;
+import com.fintech_middleware.Dashboard_Service.dto.request.AccountDto;
+import com.fintech_middleware.Dashboard_Service.dto.request.CustomerDto;
+import com.fintech_middleware.Dashboard_Service.dto.response.DashboardResponseDto;
+import com.fintech_middleware.Dashboard_Service.repository.AccountRepository;
+import com.fintech_middleware.Dashboard_Service.repository.CustomerClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+
+@Service
+public class DashboardService {
+    @Autowired
+    private  CustomerClient customerClient;
+    @Autowired
+    private  AccountRepository accountRepository;
+
+    public DashboardResponseDto getDashboard(long id) {
+        CustomerDto customer = customerClient.getCustomerById(id);
+        List<Account> accounts = accountRepository.findByCustomerId(id);
+
+        List<AccountDto> accountDtos = accounts.stream()
+                .map(a -> AccountDto.builder()
+                        .accountNumber(a.getAccountNumber())
+                        .accountType(a.getAccountType())
+                        .balance(a.getBalance())
+                        .build())
+                .collect(Collectors.toList());
+
+        return DashboardResponseDto.builder()
+                .customer(customer)
+                .accounts(accountDtos)
+                .build();
+    }
+}
